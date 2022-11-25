d941013ab9915d8e0773385b70ea3b2410ad586e
argyaWicaksana <argya7@gmail.com>
2022-11-26 06:20:46 +0700

    Delete connect.php

--- connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-    define('baseURL', explode('dashboard', $_SERVER['REQUEST_URI'])[0]);
-    $hostname = $_SERVER['SERVER_NAME'];
-    $username = 'root';
-    $password = '';
-    $database = 'YOUR_DATABASE';
-
-    $connect = mysqli_connect($hostname, $username, $password, $database);
-?>
