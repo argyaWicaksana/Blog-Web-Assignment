aa42ff59ee1c950d176610b9fc87484bb45bd5f2
argyaWicaksana <argya7@gmail.com>
2022-11-25 17:26:35 +0700

    Delete connect.php

--- process/connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-$namaHost = $_SERVER['SERVER_NAME'];
-$username = "root";
-$password = "321argya123";
-$database = "blogweb";
-
-$connect = mysqli_connect($namaHost, $username, $password, $database);
