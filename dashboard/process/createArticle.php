41bd1148e68179d7e9cb0e4cc988ae7eab99275f
Ibrahim <zakiibrahim151@gmail.com>
2022-11-28 09:53:44 +0700

    add listuser for admin role (#5)

--- dashboard/process/createArticle.php
+++ dashboard/process/createArticle.php
@@ -39,7 +39,6 @@ if (isset($_POST['submit'])) {
             }
          }
       }
-
    }
 
    if ($title != '') {
--- dashboard/users/list.php
+++ dashboard/users/list.php
@@ -1,55 +1,51 @@
-<?php include '../templates/header.php'; ?>
+<?php
+include '../templates/header.php';
+include '../../connect.php';
 
-    <h1>My Article</h1>
-    <hr>
-    <table class="table table-striped mb-5">
-        <thead>
-            <tr>
-                <th scope="col">id</th>
-                <th scope="col">User</th>
-                <th scope="col">Action</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <th scope="row">1</th>
-                <td>Twitter Was My Longest Toxic Relationship</td>
-                <td>
-                    <a href="#" class="badge bg-info">
-                        <i data-feather="eye"></i>
-                    </a>
-                    <a href="#" class="badge bg-danger">
-                        <i data-feather="trash-2"></i>
-                    </a>
-                </td>
-            </tr>
-            <tr>
-                <th scope="row">2</th>
-                <td>How to Eat Healthy</td>
-                <td>
-                    <a href="#" class="badge bg-info">
-                        <i data-feather="eye"></i>
-                    </a>
-                    <a href="#" class="badge bg-danger">
-                        <i data-feather="trash-2"></i>
-                    </a>
-                </td>
-            </tr>
-            <tr>
-                <th scope="row">3</th>
-                <td>What is Linux</td>
-                <td>
-                    <a href="#" class="badge bg-info">
-                        <i data-feather="eye"></i>
-                    </a>
-                    <a href="#" class="badge bg-danger">
-                        <i data-feather="trash-2"></i>
-                    </a>
-                </td>
-            </tr>
-        </tbody>
-    </table>
-    <a class="btn btn-success" href="create.php">Create New Article</a>
+//get title and category
+$query = "SELECT id, username, email FROM user WHERE isAdmin ='0'";
+$users = mysqli_query($connect, $query);
+// var_dump($query);
+?>
+
+<h1>My Article</h1>
+<hr>
+<table class="table table-striped mb-5">
+    <thead>
+        <tr>
+            <th scope="col">Id</th>
+            <th scope="col">username</th>
+            <th scope="col">email</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php if (mysqli_num_rows($users) > 0) {
+            $i = 1;
+            foreach ($users as $user) {
+        ?>
+                <tr>
+                    <th scope="row"><?= $i ?></th>
+                    <td><?= $user['id'] ?></td>
+                    <td><?= $user['username'] ?></td>
+                    <td><?= $user['email'] ?></td>
+                    <td>
+                        <!-- a_id means article_id -->
+                        <a href="view.php?u_id=<?= $user['id'] ?>" class="badge bg-info">
+                            <i data-feather="eye"></i>
+                        </a>
+                        <a href="../process/deleteUser.php?u_id=<?= $user['id'] ?>" class="badge bg-danger">
+                            <i data-feather="trash-2"></i>
+                        </a>
+                    </td>
+                </tr>
+        <?php
+                $i++;
+            }
+        }
+        ?>
+    </tbody>
+</table>
+<a class="btn btn-success" href="create.php">Create New Article</a>
 </article>
 </main>
 </div>
