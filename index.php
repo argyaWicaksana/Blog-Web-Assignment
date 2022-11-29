85193427e9a62a2dd8f670bc788888109b0dc57f
zm.ibrahim_ <zakiibrahim151@gmail.com>
2022-11-29 12:48:31 +0700

    Fix allignment

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php include 'templates/header.php'; 
+<?php include 'templates/header.php';
 $sql = "SELECT article_id, title, content, img, `timestamp`, username AS author, name AS category FROM article AS a
 JOIN user AS u ON(a.user_id = u.id)
 JOIN category AS c ON(a.category_id = c.id) ORDER BY `timestamp` DESC";
@@ -9,35 +9,37 @@ $articles = mysqli_query($connect, $sql);
     <h1 class="mb-4 text-center">All Post</h1>
     <div class="row">
         <?php
-            if(mysqli_num_rows($articles) > 0){
-                foreach($articles as $article){
-                    $excerpt = array_slice( explode( " ", $article['content'] ), 0, 15 );
-                    $excerpt = strip_tags(implode(' ', $excerpt)) . '...';
+        if (mysqli_num_rows($articles) > 0) {
+            foreach ($articles as $article) {
+                $excerpt = array_slice(explode(" ", $article['content']), 0, 15);
+                $excerpt = strip_tags(implode(' ', $excerpt)) . '...';
         ?>
-        <div class="col-md-4 mb-4">
-            <div class="card">
-                <?php
-                    if($article['img'] != ''){
-                ?>
-                <img src="<?= $article['img'] ?>" class="self-align-center">
-                <?php
-                    } else{
-                ?>
-                <img src="img/nopicture.jpg" style="max-height: 170px;" class="align-self-center">
-                <?php
-                    }
-                ?>
-                <div class="card-body">
-                    <h5 class="card-title"><?= $article['title'] ?></h5>
-                    <small class="card-text text-muted">By <?= $article['author'] ?> in <?= $article['category'] ?></small>
-                    <p class="card-text"><?= $excerpt ?></p>
-                    <a href="view.php?a_id=<?= $article['article_id'] ?>">Read More</a>
+                <div class="col-md-4 mb-4">
+                    <div class="card">
+                        <?php
+                        if ($article['img'] != '') {
+                        ?>
+                            <img src="<?= $article['img'] ?>" class="self-align-center">
+                        <?php
+                        } else {
+                        ?>
+                            <img src="img/nopicture.jpg" style="max-height: 170px;" class="align-self-center">
+                        <?php
+                        }
+                        ?>
+                        <div class="card-body">
+                            <a class="fs-4 card-title text-decoration-none text-capitalize" href="view.php?a_id=<?= $article['article_id'] ?>"><?= $article['title'] ?></a>
+                            <br>
+                            <!-- <h5 class="card-title">/h5> -->
+                            <small class="card-text text-muted">By <?= $article['author'] ?> in <?= $article['category'] ?></small>
+                            <p class="card-text"><?= $excerpt ?></p>
+                            <a href="view.php?a_id=<?= $article['article_id'] ?>">Read More</a>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
         <?php
-                }
             }
+        }
         ?>
     </div>
 </main>
--- view.php
+++ view.php
@@ -11,9 +11,9 @@ $article = $article->fetch_assoc();
 <main class="container mt-4">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <h2 class="mb-4 text-center"><?= $article['title'] ?></h2>
-            <div class="article_image d-flex align-items-center">
-                <img class="card-img-top" src="<?= $article['img']!='' ? $article['img'] : 'img/nopicture.jpg'?>" alt="article image">
+            <h2 class="mb-4 text-center text-capitalize"><?= $article['title'] ?></h2>
+            <div class="d-flex align-items-center">
+                <img class="card-img-top" src="<?= $article['img'] != '' ? $article['img'] : 'img/nopicture.jpg' ?>" alt="article image">
             </div>
             <small class="text-muted">By <?= $article['author'] ?> in <?= $article['category'] ?></small>
             <article class="my-3">
