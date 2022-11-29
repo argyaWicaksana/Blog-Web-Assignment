5c7aae982fd7d92efea093b652b6235e2055ae2f
zm.ibrahim_ <zakiibrahim151@gmail.com>
2022-11-29 14:06:52 +0700

    fix category
    functioning category page and sort

--- categories.php
+++ categories.php
@@ -20,13 +20,14 @@ function randomImage()
                 $excerpt = strip_tags(implode(' ', $excerpt)) . '...';
         ?>
                 <div class="col-md-4 mb-4">
-                    <div class="card">
-                        <img class="card-img-top" src="<?= randomImage() ?>" />
-                        <div class="card-body">
-                            <h5 class="card-title"><?= $category['name'] ?></h5>
-                            <a href="view.php?c_id=<?= $category['id'] ?>">Show More</a>
+                    <a href="index.php?c_id=<?= $category['id'] ?>&c_name=<?= $category['name'] ?>">
+                        <div class="card">
+                            <img class="card-img-top" src="<?= randomImage() ?>" />
+                            <div class="card-img-overlay d-flex align-items-end p-0">
+                                <h5 class="card-title text-bg-dark text-center p-4 mb-0 flex-fill bg-opacity-75"><?= $category['name'] ?></h5>
+                            </div>
                         </div>
-                    </div>
+                    </a>
                 </div>
         <?php
             }
