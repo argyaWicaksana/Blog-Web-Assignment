9ad6dbec40dea55af0173699f75f106c7a07147b
argyaWicaksana <argya7@gmail.com>
2022-12-07 21:14:03 +0700

    update kecil 🤏

--- dashboard/templates/header.php
+++ dashboard/templates/header.php
@@ -46,7 +46,7 @@ $id = $_SESSION['user_id'];
     <!-- sidebar -->
     <div class="d-flex">
         <div style="min-height: 100vh;">
-            <div class="collapse collapse-horizontal show sticky-top" id="collapseWidthExample" style="height: 100vh;">
+            <div class="collapse collapse-horizontal sticky-top" id="collapseWidthExample" style="height: 100vh;">
                 <div class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" style="width: 280px; height:100vh;">
                     <a href="<?= baseURL ?>" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
                         <span class="fs-4">Blog <span class="text-info">Web</span></span>
--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
 $(document).ready(
+    () => {
+        if ($(window).width() < 768) {
+            $('#collapseWidthExample').removeClass('show')
+        } else {
+            $('#collapseWidthExample').addClass('show')
+        }
+    },
+
     $(window).resize(() => {
         if ($(window).width() < 768) {
             $(".Article").removeClass("mx-md-5")
@@ -13,7 +21,7 @@ $(document).ready(
         }
     }),
 
-    $('#del-img').click(()=>{
+    $('#del-img').click(() => {
         let tmp__html = '<input type="hidden" name="del_img" value="1"></input>'
         $('form').append(tmp__html)
         $('#img-preview').attr('src', '')
